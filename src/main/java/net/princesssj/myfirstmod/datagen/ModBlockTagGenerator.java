25ef6cb940b99ace6a4e13a311c2ee6099f75db0
nosnowowie93347 <nosnowowie@gmail.com>
2026-07-24 22:04:39 -0700

    First Commit

---
+++ src/main/java/net/princesssj/myfirstmod/datagen/ModBlockTagGenerator.java
@@ -0,0 +1,39 @@
+package net.princesssj.myfirstmod.datagen;
+
+import net.princesssj.myfirstmod.block.ModBlocks;
+import net.princesssj.myfirstmod.util.ModTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.princesssj.myfirstmod.MyFirstMod;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagGenerator extends BlockTagsProvider {
+    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, MyFirstMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.SAPPHIRE_BLOCK.get(),
+                        ModBlocks.RAW_SAPPHIRE_BLOCK.get(),
+                        ModBlocks.SAPPHIRE_ORE.get());
+
+
+        this.tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.SAPPHIRE_BLOCK.get());
+
+        this.tag(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+
+    }
+}
+
---
+++ src/main/java/net/princesssj/myfirstmod/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.princesssj.myfirstmod.item;
+
+import net.princesssj.myfirstmod.MyFirstMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MyFirstMod.MOD_ID);
+    public static final RegistryObject<Item> PINE_CONE = ITEMS.register("pine_cone",
+            () -> new FuelItem(new Item.Properties(), 400));
+    public static final RegistryObject<Item> STRAWBERRY = ITEMS.register("strawberry",
+            () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
+
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
